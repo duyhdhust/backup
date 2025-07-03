bc72394fc3849094da665e999c3f021e39d75c4f
duyhdhust <duyhd.hust@gmail.com>
2025-07-04 00:23:07 +0700

    update

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-const baseURL = 'http://192.168.1.3:3001/api';
+const baseURL = 'http://10.1.3.152:3001/api';
 
 const api = axios.create({
     baseURL: baseURL,
